2f3b2558aeffa562a16c8a8887d3556c7fee7be7
Vessel <ianliu817@gmail.com>
2026-02-11 17:53:20 +0800

    Delete globalschoolnet directory

--- globalschoolnet/tests/value.test.js
+++
@@ -1,6 +0,0 @@
-// tests/values.test.js
-test('應能正確獲取思想品質頁面的五明心清單', () => {
-    const philosophy = getContent('philosophy');
-    expect(philosophy.hearts).toHaveLength(5);
-    expect(philosophy.hearts[0]).toContain('kind heart');
-});
